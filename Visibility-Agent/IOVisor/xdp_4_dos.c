9ea2ac1cb7f643d68739a276a4190c81c1149e5c
gist-banana <yechoe@smartx.kr>
2019-11-10 09:58:32 +0000

    working on dynamic filtering policy setup

--- Visibility-Agent/IOVisor/xdp_4_dos.c
+++ Visibility-Agent/IOVisor/xdp_4_dos.c
@@ -12,8 +12,7 @@ topicName = 'xdp_kafka_topic'
 producer = KafkaProducer(bootstrap_servers = bootstrap_servers)
 producer = KafkaProducer()
 
-def convert_ip_to_bin(data):    # converts machine friendly IP -> human friendly IP
-        #input is a machine friendly IP
+def convert_ip_to_bin(data):
         data =  "{0:b}".format(data.value).zfill(28)
         #    data = ''.join(str((int((data[0:4]),2))))
         # 0:4 - 1
@@ -28,42 +27,6 @@ def convert_ip_to_bin(data):    # converts machine friendly IP -> human friendly
         back = one +'.' + two + '.'+ three +'.' + four
         return back
 
-def convert_ip_to_machine(hum_address) :   # converts human friendly IP -> machine friendly IP
-    # input is a human friendly IP
-    length = len(hum_address)
-    dot = []    # save dot positions fo the human readable IP address
-    frag = []   # save fragmented numbers of humanm readable IP addresses, each separated by a dot
-    i=0
-    while (i< length):
-        if (hum_address[i] == '.'):
-            dot.append(i)   # save dot positions
-    i = i +1
-
-    i=0
-
-    while (i < len(dot)):   # fragment human readable IP address in between dots & switch them into binaries
-        if (i==0):
-            frag.append(bin(int(hum_address[:int(dot[0])])))
-        else:
-            frag.append(bin(int(hum_address[int(dot[i-1]+1):int(dot[i])])))
-        i = i+1
-
-    frag.append(bin(int(hum_address[int(dot[i-1])+1:])))
-    i=0
-    while (i< len(dot)+1):
-            frag[i] = frag[i][2:]
-            frag[i] = frag[i].zfill(8)
-            i = i+1
-
-    frag.reverse()  # reverse the order of the frag list
-    i=0
-    binary_ver = ''
-    while (i< len(dot)+1):
-        binary_ver = binary_ver + frag[i]
-        i = i+1
-
-    output = int(binary_ver,2)
-    return output
 
 flags = 0
 def usage():
@@ -82,7 +45,7 @@ if len(sys.argv) == 2:
 elif len(sys.argv) == 3:
     device = sys.argv[2]
 
-_xdp_file = "xdp_4_dos.c"
+_xdp_file = "backup_xdp_4_dos.c"
 
 maptype = "percpu_array"
 if len(sys.argv) == 3:
